e862005d15ab4ab66a34f4023d5f616e88e28f06
JureSeselj <jure.seselj@gmail.com>
2023-04-07 16:47:41 +0000

    Add Font Awesome link and icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,25 @@ import logo from '../assets/Harmonize-logo.png'
 const NavBar = () => {
     return (
         <Navbar expand="md" fixed="top">
-            <Container>
+        <Container>
             <Navbar.Brand>
                 <img src={logo} alt='Harmonize logo' height="60" width="80" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link>Home</Nav.Link>
-                <Nav.Link>Sign up</Nav.Link>
-                <Nav.Link>Log in</Nav.Link>
-            </Nav>
+        <Nav className="ml-auto">
+            <Nav.Link>
+                <i class="fa-solid fa-house"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+                <i class="fa-solid fa-user-plus"></i>Sign up
+            </Nav.Link>
+            <Nav.Link>
+                <i class="fa-solid fa-right-to-bracket"></i>Log in
+            </Nav.Link>
+        </Nav>
         </Navbar.Collapse>
-      </Container>
+        </Container>
     </Navbar>
   );
 };

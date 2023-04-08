d725411b004db427ba76b78e79bad48baaef0b4c
JureSeselj <jure.seselj@gmail.com>
2023-04-08 17:45:24 +0000

    Add navigation menus styles and adjust display on small screens

--- src/components/LikeFeedAddPost.js
+++ src/components/LikeFeedAddPost.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container } from "react-bootstrap";
 import appStyles from "../App.module.css";
-import styles from "../styles/LikeFeedAddPost.module.css";
+import styles from "../styles/SmallMenuContainer.module.css";
 
 
 const LikeFeedAddPost = () => {
   return (
     <Container
-      className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3 ${styles.FlexDisplay}`}
+      className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3 ${styles.LikeFeedAddDisplay}`}
           >
       <Link to="/posts/create"><i class="fa-regular fa-plus fa-fw"></i><p className={styles.Link}>Add post</p></Link>
       <Link to="/liked"><i class="fa-regular fa-heart fa-fw"></i><p className={styles.Link}>Liked posts</p></Link>

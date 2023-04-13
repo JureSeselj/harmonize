e6799a5359e45b85faa5fb9b67e613c12409993e
JureSeselj <jure.seselj@gmail.com>
2023-04-13 17:13:59 +0000

    Fix typo to useClickOutsideToggle.js file

--- src/hooks/useClickOutsideToggle.js
+++ src/hooks/useClickOutsideToggle.js
@@ -11,21 +11,19 @@ const useClickOutsideToggle = () => {
   */
   useEffect(() => {
     const handleClickOutside = (e) => {
-
       if (e.target?.classList.contains("exp")) return;
       // check if the user has clicked away from the referenced button (hamburger icon)
-      
       if (ref.current && !ref.current.contains(e.target)) {
         setExpanded(false);
       }
-    }
-    document.addEventListener("mouseup", handleClickOutside)
+    };
+    document.addEventListener("mouseup", handleClickOutside);
     return () => {
-      document.removeEventListener("mouseup", handleClickOutside)
-    }
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
   }, [ref]);
 
   return { expanded, setExpanded, ref };
-}
+};
 
-export default useClickOutsideToggle
+export default useClickOutsideToggle;
